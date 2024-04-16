3cecb1ac6063d1da8c0d728b6794f6a316d80bac
artsemm <artyomsnow@gmail.com>
2024-04-16 08:57:38 +0200

    pairing structures added

--- src/handlers/pairing.ts
+++ src/handlers/pairing.ts
@@ -2,8 +2,34 @@
 
 import * as os from 'os'
 
-function hello(name: string) {
-    return 'Hello, ' + name
+let users: Array<number> = [1, 2, 3, 4, 5, 6]
+let previousPairs: number[][] = [
+    [1, 2],
+    [5, 6],
+    [2, 4]
+]
+
+let userCounts: { [key: number]: number } = {}
+
+
+previousPairs.forEach(pair => {
+    pair.forEach(user => {
+        // If the user is in userCounts, increment its count, otherwise initialize it to 1
+        userCounts[user] = (userCounts[user] || 0) + 1
+        // If someone met each and every user, we will reset the counter
+        if (userCounts[user] == users.length) {
+            userCounts[user] = 0
+        }
+    })
+})
+
+let usersToReset: Array<number> = users.filter(user => (userCounts[user] || 0) === 0)
+
+previousPairs = previousPairs.filter(pair => 
+        !pair.some(user => usersToReset.includes(user))
+        )
+
+for (let user of users) {
+    let count = userCounts[user] || 0
+    console.log(`User ${user}: ${count}`)
 }
-const user = os.userInfo().username
-console.log(`Result: ${hello(user)}`)
