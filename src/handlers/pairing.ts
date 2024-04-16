0aa4078fd41e56b4284006f4452338d667c79072
artsemm <artyomsnow@gmail.com>
2024-04-16 09:13:29 +0200

    wrapping pairing in functions

--- src/handlers/pairing.ts
+++ src/handlers/pairing.ts
@@ -5,31 +5,33 @@ import * as os from 'os'
 let users: Array<number> = [1, 2, 3, 4, 5, 6]
 let previousPairs: number[][] = [
     [1, 2],
+    [2, 3],
     [5, 6],
-    [2, 4]
+    [2, 4],
+    [2, 5],
+    [2, 6]
 ]
 
-let userCounts: { [key: number]: number } = {}
-
-
-previousPairs.forEach(pair => {
-    pair.forEach(user => {
-        // If the user is in userCounts, increment its count, otherwise initialize it to 1
-        userCounts[user] = (userCounts[user] || 0) + 1
-        // If someone met each and every user, we will reset the counter
-        if (userCounts[user] == users.length) {
-            userCounts[user] = 0
-        }
+function countUserPairs(previousPairs: number[][]): { [key: number]: number } {
+    let userCounts: { [key: number]: number } = {}
+    previousPairs.forEach(pair => {
+        pair.forEach(user => {
+            // If the user is in userCounts, increment its count, otherwise initialize it to 1
+            userCounts[user] = (userCounts[user] || 0) + 1
+        })
     })
-})
 
-let usersToReset: Array<number> = users.filter(user => (userCounts[user] || 0) === 0)
+    return userCounts
+}
 
-previousPairs = previousPairs.filter(pair => 
+function resetPairsForLimitUsers(users: Array<number>, pairs: number[][]): number[][] {
+    let userCounts = countUserPairs(pairs)
+    let usersToReset: Array<number> = users.filter(user => (userCounts[user] || users.length - 1) === users.length - 1)
+    pairs = pairs.filter(pair => 
         !pair.some(user => usersToReset.includes(user))
         )
-
-for (let user of users) {
-    let count = userCounts[user] || 0
-    console.log(`User ${user}: ${count}`)
+    return pairs
 }
+
+let res = resetPairsForLimitUsers(users, previousPairs)
+console.log(res)
